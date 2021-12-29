72216f36b57b231a71ecf0540e5278825e44c11e
Gislain <g.gahigira@gmail.com>
2021-12-29 14:44:50 +0200

    Delete conn.php

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set("Africa/Cairo");
-$date=date('F j, Y g:i:a');
-
-$conn=mysqli_connect("localhost","root","","chatme");
-if(!$conn){
-	die("Connection failed: " . mysqli_connect_error());
-}
-?>
